41a3c3df7e0a7c0e04ecb0570116a136d50a2abf
Shane <shbburke@gmail.com>
2013-12-23 09:10:14 -0800

    created git rep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zoobook::Application.config.secret_key_base = '4e7163e5531d52faaec4f9d24204dd465505f80429318e033fdc9de90c581097bb6bd54246b5bd9029c383dcb1570d8195566c29d8608ad6d815c5fae6127493'
